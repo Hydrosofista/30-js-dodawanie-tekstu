c18f9579a61930cbe187fac25365c075c052e9d7
Hydrosofista <hydrosofista@gmail.com>
2024-05-16 16:11:31 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+
+let myParagraph= document.querySelector('p');
+myParagraph.textContent = ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, necessitatibus. Eaque corrupti exercitationem praesentium ipsam possimus voluptate dolore! Velit deleniti necessitatibus natus eaque dolor libero sunt labore dignissimos fugit earum.';
+console.log(myParagraph);
